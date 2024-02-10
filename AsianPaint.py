caf09532144bbefb490736b1f1d302a65a30499e
Ajeet Bisht <40465861+Ajeetbisht@users.noreply.github.com>
2024-02-10 13:48:00 +0530

    Update AsianPaint.py

--- AsianPaint.py
+++ AsianPaint.py
@@ -1,109 +1,182 @@
+import tkinter as tk
+from tkinter import filedialog, Entry, messagebox
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 import time
 import csv
-import tkinter as tk
-from tkinter import filedialog
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 
+counter = 0
 
-def import_csv_file():
-    csv_file_path = filedialog.askopenfilename(title="Open CSV File", filetypes=[("CSV files", "*.csv")])
-    return csv_file_path
-
-def execute_asian_automation(line, url, delay_page, delay_scroll1, data_delay, submit_page):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    driver.maximize_window()
-    time.sleep(delay_page)
-
-    # Close popup
-    accept_cookies = driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]').click()
-
-    # Scroll
-    driver.execute_script("window.scrollTo(0, 1800)")
-    time.sleep(delay_scroll1)
-    driver.execute_script("window.scrollTo(0, 0)")
-
+def automate_data_submission(line, driver):
     try:
-        wait = WebDriverWait(driver, 2)
-
-        #access fields
         username_input = driver.find_element(By.ID, 'enquire-name')
         email_input = driver.find_element(By.ID, 'enquire-email')
         mobile_input = driver.find_element(By.ID, 'enquire-mobile')
         pincode_input = driver.find_element(By.ID, 'enquire-pincode')
-        submit_button = driver.find_element(By.CLASS_NAME, 'ctaText')
 
-        #send data
         username_input.send_keys(line[0])
         email_input.send_keys(line[1])
         mobile_input.send_keys(line[2])
         pincode_input.send_keys(line[3])
 
-        remover = driver.execute_script("document.querySelectorAll('button')[1].classList.remove('otpValidate');")
+        remover = driver.execute_script("document.querySelectorAll('button')[0].classList.remove('otpValidate');")
+        data_delay = float(entry_data_delay.get())
         time.sleep(data_delay)
         submit = pincode_input.send_keys(Keys.ENTER)
 
-        time.sleep(submit_page)
-        
-        if submit:
-            print("Response submitted, thank you!")
-        else:
-            print("Not submitted")
-    except Exception as e:
-        print(e)
+        # Check if the success element is present
+        success_element = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div[1]/div/div/div/h2"))
+        )
+        time.sleep(2)
 
+        if success_element:
+            global counter
+            counter += 1
+            print("Data send:", counter)
+            newfile = data_file.get()
+            with open(f'{newfile}.csv', 'a', newline='') as success_file:
+                success_writer = csv.writer(success_file)
+                datasuccess = int(1)
+                user_data = [line[0], line[1], line[2], line[3], datasuccess]
+                success_writer.writerow(user_data)
+            return True
+        else:
+            print('Data send failed')
+            return False
+
+    except NoSuchElementException as e:
+        print(f"Element not found: {e}")
+        return False
+    except TimeoutException as e:
+        print(f"Timeout: {e}")
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+
+def automate_asianpaint():
+    global counter
+    try:
+        url = entry_url.get()
+        file_path = filedialog.askopenfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv"), ("All Files", "*.*")])
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("start-maximized")
+        options.add_argument("--incognito")
+        driver = webdriver.Chrome(options=options)
+
+#        driver.get("http://192.168.8.1/html/home.html")
+        driver.get(dongal_url.get())
+        driver.maximize_window()
+
+        with open(file_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            next(csv_reader)
+
+            for line in csv_reader:
+                changeIp = driver.find_element(By.XPATH, dongal_xpath.get())
+#                changeIp = driver.find_element(By.XPATH, '//*[@id="mobile_connect_btn"]')
+                changeIp.click()
+                time.sleep(1)
+
+                driver.execute_script("window.open('', '_blank');")
+                driver.switch_to.window(driver.window_handles[-1])
+                driver.get(entry_url.get())
+#                driver.get('https://www.asianpaints.com/campaign/ap-crm/index.html')
+                delay_page = float(entry_delay_page.get())
+                time.sleep(delay_page)
+
+                # Scroll
+                driver.execute_script("window.scrollTo(0, 1500)")
+                delay_scroll1 = float(entry_delay_scroll1.get())
+                time.sleep(delay_scroll1)
+                driver.execute_script("window.scrollTo(0, 0)")
+                delay_scroll2 = float(entry_delay_scroll2.get())
+                time.sleep(delay_scroll2)
+
+                success = automate_data_submission(line, driver)
+
+                driver.delete_all_cookies()
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                changeIp.click()
+
+                dongal = dongal_delay.get()
+                time.sleep(int(dongal))
+
+                if success:
+                    print("data send:", counter)
+                else:
+                    print('data send fail:')
+            print("Automation completed")
+
+    except Exception as e:
+        print("Error", f"An error occurred: {str(e)}")
     finally:
         driver.quit()
-        print("Browser quits")
 
 
-def Challo():
-    csv_file_path = import_csv_file()
-    url = entry_url.get()
-    delay_page = float(entry_delay_page.get())
-    delay_scroll1 = float(entry_delay_scroll1.get())
-    data_delay = float(entry_data_delay.get())
-    submit_page = float(entry_submit_page.get())
-
-    with open(csv_file_path, 'r') as csv_file: 
-        csv_reader = csv.reader(csv_file)
-        next(csv_reader)
-        
-        for line in csv_reader:
-            execute_asian_automation(line, url, delay_page, delay_scroll1, data_delay, submit_page)
+#------------------- GUI -----------------------
+root = tk.Tk()
+root.title('AsianPaint Automation')
+root.geometry('580x400+300+200')
 
 
-# -------------- GUI -------------------
-r = tk.Tk()
-r.title('Asianpaint Automation')
-r.geometry('500x400+300+200')
+tk.Label(root, text='AsianPaint form filling automation', font=('Helvetica', 14, 'bold')).grid(row=0, column=1)
 
-tk.Label(r, text='Enter Asianpaint Tracking Link:').grid(row=0, column=0)
-entry_url = tk.Entry(r, width=38)
-entry_url.grid(row=0, column=1)
+tk.Label(root, text='Dongal to change I.P', font=('Helvetica', 10, 'bold')).grid(row=1, column=1)
 
-tk.Label(r, text='Delay After Page Load (seconds):').grid(row=1, column=0)
-entry_delay_page = tk.Entry(r, width=10)
-entry_delay_page.grid(row=1, column=1)
+tk.Label(root, text='Enter Dongal Link :', font=('Helvetica', 10)).grid(row=2, column=0)
+dongal_url = Entry(root, width=47)
+dongal_url.grid(row=2, column=1)
 
-tk.Label(r, text='Delay after 1st Scroll (seconds):').grid(row=2, column=0)
-entry_delay_scroll1 = tk.Entry(r, width=10)
-entry_delay_scroll1.grid(row=2, column=1)
+tk.Label(root, text='Enter Xpath of button :', font=('Helvetica', 10)).grid(row=3, column=0)
+dongal_xpath = tk.Entry(root, width=50)
+dongal_xpath.grid(row=3, column=1)
 
-tk.Label(r, text='Delay for Data submit time (seconds):').grid(row=4, column=0)
-entry_data_delay = tk.Entry(r, width=10)
-entry_data_delay.grid(row=4, column=1)
+tk.Label(root, text='Enter Dongal Delay time :', font=('Helvetica', 10)).grid(row=4, column=0)
+dongal_delay = tk.Entry(root, width=10)
+dongal_delay.grid(row=4, column=1)
 
-tk.Label(r, text='Wait Time Post Submit Botton (seconds):').grid(row=5, column=0)
-entry_submit_page = tk.Entry(r, width=10)
-entry_submit_page.grid(row=5, column=1)
+separator = tk.Frame(height=2, bd=1, relief="groove")
+separator.grid(row=5, columnspan=2, sticky="ew", pady=5)
 
-tk.Label(r, text='Upload only CSV file').grid(row=6, column=0)
-tk.Button(r, text="Automate Colgate", command=Challo, height=2, width=30).grid(row=7, column=1)
 
-tk.Button(r, text='Close Script', command=r.destroy).grid(row=8, column=1, columnspan=2)
-r.mainloop()
+tk.Label(root, text='AsianPaint Automation Section: ', font=('Helvetica', 10, 'bold')).grid(row=6, column=1)
+tk.Label(root, text='Enter AsianPaint Tracking Link: ', font=('Helvetica', 10)).grid(row=7, column=0)
+entry_url = Entry(root, width=50)
+entry_url.grid(row=7, column=1)
+
+tk.Label(root, text='Delay After Page Load (seconds) :', font=('Helvetica', 10)).grid(row=8, column=0)
+entry_delay_page = tk.Entry(root, width=10)
+entry_delay_page.grid(row=8, column=1)
+
+tk.Label(root, text='Delay after 1st Scroll (seconds) :', font=('Helvetica', 10)).grid(row=9, column=0)
+entry_delay_scroll1 = tk.Entry(root, width=10)
+entry_delay_scroll1.grid(row=9, column=1)
+
+tk.Label(root, text='Delay after 2nd Scroll (seconds) :', font=('Helvetica', 10)).grid(row=10, column=0)
+entry_delay_scroll2 = tk.Entry(root, width=10)
+entry_delay_scroll2.grid(row=10, column=1)
+
+tk.Label(root, text='Delay for Data submit time (seconds) :', font=('Helvetica', 10)).grid(row=11, column=0)
+entry_data_delay = tk.Entry(root, width=10)
+entry_data_delay.grid(row=11, column=1)
+
+separator = tk.Frame(height=2, bd=1, relief="groove")
+separator.grid(row=12, columnspan=2, sticky="ew", pady=5)
+
+tk.Label(root, text='Enter data send file name:', font=('Helvetica', 10)).grid(row=13, column=0)
+data_file = tk.Entry(root, width=35)
+data_file.grid(row=13, column=1)
+
+tk.Button(root, text="Automate AsianPaint", command=automate_asianpaint, height=2, width=30, font=('Helvetica', 12, 'bold')).grid(row=14, column=1)
+
+tk.Button(root, text='Close', command=root.destroy).grid(row=15, column=1)
+root.mainloop()
